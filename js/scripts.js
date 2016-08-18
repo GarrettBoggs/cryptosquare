5592870ab413bc354f53a5388d83a1c8f912a651
David Bethune and Garrett Boggs <garrett@>
2016-08-18 13:23:42 -0700

    add spaces when necessary

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,15 @@ var result = "";
 var add = "";
 var counter = 12;
 var increment = 6;
+var row = 0;
+var col = 0;
 var newArray = [];
-var firstArray = [];
 
 //back-end logic
 var convert = function(data)
 {
+  newArray = [];
+  firstArray = [];
   firstArray = data.split("");
 
   for(var i = 0; i < firstArray.length ; i ++)
@@ -31,26 +34,32 @@ $(document).ready(function() {
     if(newArray.length >= 48)
     {
       increment = 1.8;
+      col = 7;
     }
     else if(newArray.length >= 35)
     {
       increment = 2;
+      col = 6;
     }
     else if(newArray.length >= 24)
     {
       increment = 2.5;
+      col = 5;
     }
     else if(newArray.length >= 15)
     {
       increment = 3;
+      col = 4;
     }
     else if(newArray.length >= 9)
     {
       increment = 4;
+      col = 3;
     }
     else
     {
       increment = 6;
+      col = 2;
     }
 
     for(var i = 0; i < newArray.length; i ++)
@@ -61,9 +70,14 @@ $(document).ready(function() {
       {
         counter = 0;
         $("#output").append("<div class=\"row\">");
+        row += 1;
       }
         $("#output").append("<div class=\"col-md-1\">" + newArray[i] + "</div>");
+    }
 
+    for(var j = 0; j < (col*row) - newArray.length; j++)
+    {
+      $("#output").append("<div class=\"col-md-1\">" + "&nbsp;" + "</div>");
     }
   });
 });
